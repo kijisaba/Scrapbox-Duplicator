46bace16fc41478e91a3e06a97ae051c2c44fe55
kijisaba <>
2026-07-05 14:31:46 +0900

    ###fix-duplicator-yml deno fmtの対象からREADME.mdを除外

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-import 'https://deno.land';
-export { assertString } from 'https://deno.land';
-export * from 'jsr:@cosense/std/rest/page-data.ts';
+import "https://deno.land";
+export { assertString } from "https://deno.land";
+export * from "jsr:@cosense/std/rest/page-data.ts";
